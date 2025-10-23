bcb56914ee48385fb041a69ecc22cb3dcd764856
makotopin <makotopin1016@gmail.com>
2025-10-23 15:17:27 +0900

    composerライブラリのCarbonをインストールして使ってみた

--- 20251021-22/index.php
+++ 20251021-22/index.php
@@ -6,3 +6,9 @@ use App\Controllers\TestController;
 
 $app = new TestController;
 $app->run();
+
+
+use Carbon\Carbon;
+
+echo Carbon::now()->format('Y-m-d H:i:s');
+// composerにはCarbon以外にもたくさんのライブラリがある
